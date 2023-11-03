904ed88ee00a2aa7612dbb8227edbdfa1d2228ac
André Azevêdo <andreazevedoluiz@gmail.com>
2023-11-03 19:19:57 -0300

    Scafold

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,84 +17,13 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Container(
-          color: Colors.white,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly, // vertical
-            crossAxisAlignment: CrossAxisAlignment.center, //orizontal
-            children: [
-              Stack(
-                alignment: AlignmentDirectional.center,
-                children: [
-                  Container(
-                    color: Colors.black,
-                    width: 100,
-                    height: 100,
-                  ),
-                  Container(
-                    color: Colors.red,
-                    width: 50,
-                    height: 50,
-                  ),
-                ],
-              ),
-              Stack(
-                alignment: AlignmentDirectional.center,
-                children: [
-                  Container(
-                    color: Colors.red,
-                    width: 100,
-                    height: 100,
-                  ),
-                  Container(
-                    color: Colors.black,
-                    width: 50,
-                    height: 50,
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    color: Colors.cyan,
-                    height: 50,
-                    width: 50,
-                  ),
-                  Container(
-                    color: Colors.pinkAccent,
-                    height: 50,
-                    width: 50,
-                  ),
-                  Container(
-                    color: Colors.purple,
-                    height: 50,
-                    width: 50,
-                  ),
-                ],
-              ),
-              Container(
-                color: Colors.amberAccent,
-                height: 30,
-                width: 300,
-                child: Text(
-                  'Textando textos',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 28,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  print('VOCÊ APERTOU O BOTÃO');
-                },
-                child: Text('APERTE!'),
-              ),
-            ],
-          )),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Flutter meus primeiros passos"),
+        ),
+        body: Container(),
+        floatingActionButton: FloatingActionButton(onPressed: () {}),
+      ),
     );
   }
 }
