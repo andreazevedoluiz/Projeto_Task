e72f8f6ab8fddc5ad1382f3da99c9d05f9688ff0
André Luiz Azevêdo <andreazevedoluiz@gmail.com>
2023-11-08 16:55:02 -0300

    criando pasta assets/images

--- lib/screens/initial_screen.dart
+++ lib/screens/initial_screen.dart
@@ -23,22 +23,13 @@ class _InitialScreenState extends State<InitialScreen> {
         duration: const Duration(milliseconds: 1000),
         child: ListView(
           children: const [
-            Task(
-                'Aprender flutter',
-                'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
-                5),
-            Task(
-                'Andar de bike',
-                'https://tswbike.com/wp-content/uploads/2020/09/108034687_626160478000800_2490880540739582681_n-e1600200953343.jpg',
-                2),
-            Task(
-                'Meditar',
-                'https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg',
-                3),
-            Task(
-                'Ler',
-                'https://thebogotapost.com/wp-content/uploads/2017/06/636052464065850579-137719760_flyer-image-1.jpg',
-                1),
+            Task('Aprender flutter', 'assets/images/mascote.png', 5),
+            Task('Andar de bike', 'assets/images/bike.jpg', 2),
+            Task('Meditar', 'assets/images/meditacao.jpeg', 3),
+            Task('Ler', 'assets/images/ler.jpg', 1),
+            SizedBox(
+              height: 80,
+            )
           ],
         ),
       ),
