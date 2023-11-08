a4caeb55e9c6938687479c8e75287ccff290ee02
André Luiz Azevêdo <andreazevedoluiz@gmail.com>
2023-11-08 14:57:51 -0300

    teste2

--- lib/minha_primeira_tela.dart
+++ lib/minha_primeira_tela.dart
@@ -1,79 +1,88 @@
-/*Container(
-          color: Colors.white,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly, // vertical
-            crossAxisAlignment: CrossAxisAlignment.center, //orizontal
-            children: [
-              Stack(
-                alignment: AlignmentDirectional.center,
-                children: [
-                  Container(
-                    color: Colors.black,
-                    width: 100,
-                    height: 100,
-                  ),
-                  Container(
-                    color: Colors.red,
-                    width: 50,
-                    height: 50,
-                  ),
-                ],
-              ),
-              Stack(
-                alignment: AlignmentDirectional.center,
-                children: [
-                  Container(
-                    color: Colors.red,
-                    width: 100,
-                    height: 100,
-                  ),
-                  Container(
-                    color: Colors.black,
-                    width: 50,
-                    height: 50,
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    color: Colors.cyan,
-                    height: 50,
-                    width: 50,
-                  ),
-                  Container(
-                    color: Colors.pinkAccent,
-                    height: 50,
-                    width: 50,
-                  ),
-                  Container(
-                    color: Colors.purple,
-                    height: 50,
-                    width: 50,
-                  ),
-                ],
-              ),
-              Container(
-                color: Colors.amberAccent,
-                height: 30,
-                width: 300,
-                child: Text(
-                  'Textando textos',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 28,
-                  ),
-                  textAlign: TextAlign.center,
+import 'package:flutter/material.dart';
+
+class MyFirstWidget extends StatelessWidget {
+  const MyFirstWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly, // vertical
+          crossAxisAlignment: CrossAxisAlignment.center, //orizontal
+          children: [
+            Stack(
+              alignment: AlignmentDirectional.center,
+              children: [
+                Container(
+                  color: Colors.black,
+                  width: 100,
+                  height: 100,
                 ),
+                Container(
+                  color: Colors.red,
+                  width: 50,
+                  height: 50,
+                ),
+              ],
+            ),
+            Stack(
+              alignment: AlignmentDirectional.center,
+              children: [
+                Container(
+                  color: Colors.red,
+                  width: 100,
+                  height: 100,
+                ),
+                Container(
+                  color: Colors.black,
+                  width: 50,
+                  height: 50,
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  color: Colors.cyan,
+                  height: 50,
+                  width: 50,
+                ),
+                Container(
+                  color: Colors.pinkAccent,
+                  height: 50,
+                  width: 50,
+                ),
+                Container(
+                  color: Colors.purple,
+                  height: 50,
+                  width: 50,
+                ),
+              ],
+            ),
+            Container(
+              color: Colors.amberAccent,
+              height: 30,
+              width: 300,
+              child: Text(
+                'Textando textos',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 28,
+                ),
+                textAlign: TextAlign.center,
               ),
-              ElevatedButton(
-                //CRIANDO UM BOTÃO
-                onPressed: () {
-                  print('VOCÊ APERTOU O BOTÃO');
-                },
-                child: Text('APERTE!'),
-              ),
-            ],
-          )),*/
+            ),
+            ElevatedButton(
+              //CRIANDO UM BOTÃO
+              onPressed: () {
+                print('VOCÊ APERTOU O BOTÃO');
+              },
+              child: Text('APERTE!'),
+            ),
+          ],
+        ));
+  }
+}
