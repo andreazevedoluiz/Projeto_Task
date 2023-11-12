f533d342850ee1f05e48acba0d6025867af1c75e
André Luiz Azevêdo <andreazevedoluiz@gmail.com>
2023-11-12 14:41:55 -0300

    Visualizando imagem em tempo real

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -63,6 +63,11 @@ class _FormScreenState extends State<FormScreen> {
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: TextFormField(
+                  onChanged: (text){
+                    setState(() {
+                      
+                    });
+                  } ,
                   controller: imageController,
                   textAlign: TextAlign.center,
                   decoration: InputDecoration(
@@ -71,6 +76,21 @@ class _FormScreenState extends State<FormScreen> {
                     fillColor: Colors.white70,
                     filled: true,
                   ),
+                ),
+              ),
+              Container(
+                height: 100,
+                width: 72,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(width: 2, color: Colors.blue),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.network(imageController.text, 
+                  fit: BoxFit.cover,),
+
                 ),
               ),
               ElevatedButton(
