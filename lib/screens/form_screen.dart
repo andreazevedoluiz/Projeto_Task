9e87198a8c8d1bba8b63b42d77f18fe74b2c43a5
André Luiz Azevêdo <andreazevedoluiz@gmail.com>
2023-11-12 14:31:25 -0300

    controller

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -1,43 +1,88 @@
 import 'package:flutter/material.dart';
 
-class FormScreen extends StatelessWidget {
+class FormScreen extends StatefulWidget {
   const FormScreen({super.key});
 
   @override
-  //State<formaScreen> createState() => _formaScreenS();
-  // class _FormaScreenState> createSatate() => _FormScreens(runtimeType)>
+  _FormScreenState createState() => _FormScreenState();
+}
 
+class _FormScreenState extends State<FormScreen> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController difficultyController = TextEditingController();
+  TextEditingController imageController = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-       leading: Container(),
+        leading: Container(),
         backgroundColor: Colors.blue,
-        title: const Text("Nova Tarefa", 
-        style: TextStyle(color: Colors.white),
-        ), 
+        title: const Text(
+          "Nova Tarefa",
+          style: TextStyle(color: Colors.white),
+        ),
       ),
       body: Center(
         child: Container(
           height: 580,
           width: 390,
-          decoration: BoxDecoration(color:  Colors.black12,
-          borderRadius: BorderRadius.circular(12),
-          border: Border.all(width: 3)),
-          child: Column(children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextFormField(
-                textAlign: TextAlign.center,
-                decoration:InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Nome',
-                  fillColor: Colors.white70,
-                  filled: true,
-                 ),
+          decoration: BoxDecoration(
+            color: Colors.black12,
+            borderRadius: BorderRadius.circular(12),
+            border: Border.all(width: 3),
+          ),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  controller: nameController,
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Nome',
+                    fillColor: Colors.white70,
+                    filled: true,
+                  ),
+                ),
               ),
-            ),
-
-          ]),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  controller: difficultyController,
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Dificuldade ',
+                    fillColor: Colors.white70,
+                    filled: true,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  controller: imageController,
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Imagem',
+                    fillColor: Colors.white70,
+                    filled: true,
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  print(nameController.text);
+                  print(int.parse(difficultyController.text));
+                  print(imageController);
+                },
+                child: Text('Adicionar'),
+              ),
+            ],
+          ),
         ),
       ),
     );
